45b16ee056393fbbe7ff9114671af41844d7cbbe
zG <22377978+zenGator@users.noreply.github.com>
2022-11-28 09:49:55 -0500

    Add files via upload

---
+++ Decent perl template.pl
@@ -0,0 +1,136 @@
+#!/usr/bin/perl
+# https://github.com/zenGator/ [name]
+# zG:20221126
+
+# other notes
+=for comment 
+Comment text, spanning multiple lines. 
+It ends with the first blank line.
+
+=cut
+
+=begin comment
+Multi-paragraph comment section.
+Which is terminated by an =end comment on a line by itself.
+
+This is a second paragraph.  Note that the blank lines are part of the syntax and the "=" must be the initial characters.
+
+=end comment
+
+=cut
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#use and constants here
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+use strict;
+use warnings;
+use Getopt::Std;
+#use File::Copy;
+#use constant XWFLIM => 100000;
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#declare subroutines here
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#sub usage;
+#sub capOutput;
+
+# switches followed by a : expect an argument
+# see usage() below for explanation of switches
+my $commandname=$0=~ s/^.*\///r;
+my %opt=();
+getopts('hi:o:', \%opt) or usage();
+
+
+usage () if ( $opt{h} or (scalar keys %opt) == 0 ) ;
+
+=begin method 
+#ensure options needed for others are provided  (in this case, s requires o)
+if ( $opt{s} and ! $opt{o}) {
+    print "ERROR ($commandname):  -o required when using -s.\n\n";
+    usage();
+    }
+
+=end method
+
+=cut
+
+
+my $infile=$opt{i};
+open(my $fh, '<:encoding(UTF-8)', $infile)
+  or die "Could not open file '$infile' $!";
+
+my $outFH=*STDOUT;
+if ($opt{o}) {
+    open($outFH, '>:encoding(UTF-8)', $opt{o}) 
+        or die "Could not open file '$opt{o}': $!\n";
+    }
+
+=begin method
+my $logfile=*STDERR;
+if ($opt{l}) {
+    open($logfile, '>:encoding(UTF-8)', $opt{l}) 
+        or die "Could not open file '$opt{l}': $!\n";
+    }
+*STDERR=$logfile;
+
+=end method
+
+=cut
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#declare variables here
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~    
+my $x=0;  #primary counter, the line we are currently processing
+my $outFiCount=0;
+
+
+while (my $row = <$fh>) {
+    #get a line
+    $x++;  # increment line counter
+    $regEx=0;
+    chomp $row;  #drop the newline at the end of the row
+    my $orig_row=$row;	#store this off for later reference
+
+    # transforms
+    $row =~ s/([^\\]\[[^]]*)\\d([^]]*\])/$1#$2/g;
+    $row =~ s/\\d/#/g;
+
+    if ( $row =~ /[^\\]\[[^]]*\\b[^\]]*\]/ ) {
+    warn "word boundary inside braces (could be problematic):  ## on line $x\n";
+    }
+    $row =~ s/\\b/[^_a-z#]/g;
+    $row =~ s/([^\\]\[[^]]*)\\W([^\]]*\])/$1\\x21-\\x2F\\x3A-\\x40\\x5B-\\x60\\x7B-\\x7E$2/g;
+    $row =~ s/\\W/[\\x21-\\x2F\\x3A-\\x40\\x5B-\\x60\\x7B-\\x7E]/g;
+    $row =~ s/,\}/,9\}/g;
+
+#show results
+            capOutput($reFiCount++,length($row),$refile, $reFH);
+        $chars+=length($row)+2; # the 2 is for the \r\n
+        printf $outFH "%s\r\n", $row;
+        }
+    
+}
+
+sub usage() {
+    print "like this: \n\t".$commandname." -i [infile] -o [outfile] [-l [logfile]] [-s]\n";
+    print "\nThis adjusts RegEx (as used in mwscan, possibly POSIX-compliant) into XWF-compatible RegEx/grep strings.  Because XWF has a limit of ".XWFLIM." characters for any set of simultaneous-search strings, if the output file reaches that limit, multiple output files are created by appending a digit (zero-indexed, of course) to the output file name.  Each will need to be run as a separate simultaneous search.\n";
+    print "\nThe -s switch will [s]plit the output into two [sets of] files:  one that works as simple string search terms and another that contains RegEx terms which will require the 'GREP syntax' option be selected in XWF.  You must identify an output file if using -s.\n";
+    exit 1;
+    }
+
+sub capOutput {
+    # XWF can only ingest XWFLIM chars for simul-search
+    my $count=shift;
+    my $len=shift;
+    my $file=shift;
+    my $FH=shift;
+    printf STDERR "XWF limit reached, saving output file segment as: %s\n",$file."_".$count;
+    close $FH;
+    move($file,$file."_".$count) or die "Couldn't rename '$file': $!\n";
+    if ($regEx){
+        open($FH, '>:encoding(UTF-8)', $file) or die "Could not open file '$file' $!";
+        }
+    else {
+        open($FH, '>:encoding(UTF-8)', $file) or die "Could not open file '$file' $!";
+        }
+    }
